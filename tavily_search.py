2b7044cd333e6e5f6440d2031ed6478ce6093979
asa1997 <arunsureshampadath@gmail.com>
2025-06-15 20:34:28 +0530

    updated

--- tavily_search.py
+++ tavily_search.py
@@ -1,8 +1,9 @@
+import os
 from tavily import TavilyClient
 
 # Step 1. Instantiating your TavilyClient
 api_key = os.environ.get("TAVILY_API_KEY")
-tavily_client = TavilyClient(api_key="tvly-YOUR_API_KEY")
+tavily_client = TavilyClient(api_key=api_key)
 
 # Step 2. Executing a simple search query
 response = tavily_client.search("Who is Leo Messi?")
